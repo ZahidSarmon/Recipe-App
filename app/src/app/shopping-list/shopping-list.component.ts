92bb2cf4660e6cb4fd6d4d58f1bbc29e634811e0
zahidsarmon <hashemzahid@yandex.com>
2022-08-20 21:44:44 +0600

    shopping-list-work

--- app/src/app/shopping-list/shopping-list.component.ts
+++ app/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingradientList:ingredient[]=[
+    new ingredient('Apple',10),
+    new ingredient('Banana',20),
+    new ingredient('Orange',30),
+  ];
   constructor() { }
 
   ngOnInit(): void {
